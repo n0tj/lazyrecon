daea0046688f3b0392d6dc2db72090e031c6fccb
n0tj <jay.lansiquot@icloud.com>
2019-03-24 23:58:04 -0400

    cleaned up readme and included install.sh for tools

---
+++ install.sh
@@ -0,0 +1,108 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install -y git
+sudo apt-get install rename
+sudo apt-get install -y python3-pip
+
+apt install -y python-pip
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+
+
+#Don't forget to set up AWS credentials!
+echo "Don't forget to set up AWS credentials!"
+apt install -y awscli
+echo "Don't forget to set up AWS credentials!"
+
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+
+#create a tools folder in ~/
+mkdir ~/tools
+cd ~/tools/
+
+
+echo "installing JSParser"
+git clone https://github.com/nahamsec/JSParser.git
+cd JSParser*
+sudo python setup.py install
+cd ~/tools/
+echo "done"
+
+echo "installing Sublist3r"
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r*
+pip install -r requirements.txt
+cd ~/tools/
+echo "done"
+
+
+echo "installing teh_s3_bucketeers"
+git clone https://github.com/tomdev/teh_s3_bucketeers.git
+cd ~/tools/
+echo "done"
+
+
+echo "installing wpscan"
+git clone https://github.com/wpscanteam/wpscan.git
+cd wpscan*
+sudo gem install bundler && bundle install --without test
+cd ~/tools/
+echo "done"
+
+echo "installing dirsearch"
+git clone https://github.com/maurosoria/dirsearch.git
+cd ~/tools/
+echo "done"
+
+
+echo "installing lazys3"
+git clone https://github.com/nahamsec/lazys3.git
+cd ~/tools/
+echo "done"
+
+echo "installing virtual host discovery"
+git clone https://github.com/jobertabma/virtual-host-discovery.git
+cd ~/tools/
+echo "done"
+
+echo "installing web screenshot"
+git clone https://github.com/maaaaz/webscreenshot.git
+cd ~/tools/
+echo "done"
+
+echo "installing bash_profile aliases from recon_profile"
+git clone https://github.com/nahamsec/recon_profile.git
+cd recon_profile
+cat bash_profile >> ~/.bash_profile
+source ~/.bash_profile
+cd ~/tools/
+echo "done"
+
+echo "installing sqlmap"
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+cd ~/tools/
+echo "done"
+
+echo "installing knock.py"
+sudo apt-get install python-dnspython
+git clone https://github.com/guelfoweb/knock.git
+cd ~/tools/
+echo "done"
+
+echo "installing lazyrecon"
+git clone https://github.com/nahamsec/lazyrecon.git
+cd ~/tools/
+echo "done"
+
+echo "installing nmap"
+sudo apt-get install nmap
+echo "done"
+
+echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
+ls -la
+echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
